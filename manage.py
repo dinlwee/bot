056e2743fa558255854bc02cefad0a1ffea7c54d
dinlwee <dinagacenko@gmail.com>
2022-10-27 16:36:22 +0300

    initial commit

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
@@ -6,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ddj.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LYMS.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,3 +21,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
